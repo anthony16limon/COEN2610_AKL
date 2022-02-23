d0b3ba2cde14b67c7af51a8e96667c26bdc1eaac
anthony16limon <98725380+anthony16limon@users.noreply.github.com>
2022-02-23 15:52:17 -0600

    Add files via upload

---
+++ find3.c
@@ -0,0 +1,40 @@
+/* find3.c - Open file and count the number of '3' characters found 
+   @MarquetteU  - A. Limon
+   COEN 2610
+   To compile: cc -g find3.c -o find3
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+    FILE *fd;
+    const char *filename = {"lambeau.jpg"};
+    int i, file_size;
+    int seven_count = 0;
+    unsigned char value;
+
+    /* Open file in binary mode for reading */
+    fd = fopen(filename, "rb");
+    if(fd == NULL)
+    {
+        printf("Error!");   
+        exit(1);             
+    }
+
+    /* Get size in bytes */
+    fseek(fd, 0, SEEK_END);
+    file_size = ftell(fd);
+
+    /* Start at the begining of the file */ 
+    fseek(fd, 0, SEEK_SET);
+    /* Test each byte to count 3's */
+    for( i = 0; i< file_size; i++)
+    {
+       fread(&value, 1, 1, fd);
+       if( value == '3') seven_count++; 
+    }
+    fclose(fd);
+    printf("Number of '3' characters found in %s = %d\n", filename, seven_count);
+    exit(0);
+}
